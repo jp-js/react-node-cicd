1f0caf2254a26a3425bc00c1cc33600b08e5fb38
jp-js <jsinghkik@gmail.com>
2023-07-16 23:03:35 +0530

    add github actions

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname,'/frontend/build')));
 app.get('/names',(req,res) => {
-    res.send("Telmo");
+    res.send("Jagseer Singh");
 })
 app.listen(5000, () => {
     console.log('====================================');
